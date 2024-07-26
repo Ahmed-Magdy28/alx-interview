59effe7088e40f3c5a024dcfc273845b3bbc46a9
Ahmed-Magdy28 <ray2legand@gmail.com>
2024-07-26 21:23:04 +0300

    fixing pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def pascal_triangle(n):
     """
     Generate Pascal's Triangle up to the nth row.
@@ -12,7 +13,7 @@ def pascal_triangle(n):
     """
     if n <= 0:  # If n is less than or equal to 0, return an empty list.
         return []
-    Triangle = []  # Initialize an empty list to hold the rows of Pascal's Triangle.
+    Triangle = []
     for i in range(n):  # Iterate over the range from 0 to n-1.
         if i == 0:
             Triangle.append([1])  # The first row is always [1].
@@ -20,12 +21,11 @@ def pascal_triangle(n):
             Triangle.append([1, 1])  # The second row is always [1, 1].
         else:
             row = [1]  # Start each new row with 1.
-            for x in range(1, len(Triangle[i - 1])):  # Iterate over the indices of the previous row.
-                # Append the sum of the two elements above the current position.
+            for x in range(1, len(Triangle[i - 1])):
+                # Append the sum of the two elements above the current
                 row.append(Triangle[i - 1][x] + Triangle[i - 1][x - 1])
             row.append(1)  # End each row with 1.
-            Triangle.append(row.copy())  # Append the complete row to the Triangle.
-            row.clear()  # Clear the row list to prepare for the next iteration.
+            Triangle.append(row.copy())
+            row.clear()  # Clear the row list
 
     return Triangle  # Return the complete Pascal's Triangle.
-
